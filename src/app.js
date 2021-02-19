5c8c5895d1601c37f39834d8c202b5c473894f4a
Soary <tolojanaharysoary@gmail.com>
2021-02-19 18:02:17 +0300

    Installation and configuration of module cors and helmet

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 require('./db/db')
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 
 const siteRouter = require('./routes/Site')
 const userRouter = require('./routes/User')
 
 const app = express()
 
+app.use(helmet())
+app.use(cors())
 app.use(express.json())
 
 app.use(siteRouter)
